16ae5c54c26f2f194f49ba4f0af1a288079e3198
GabriHR <ghernrod@myuax.com>
2024-04-24 00:18:18 +0200

    17

---
+++ src/main/java/Analisis_Organizacion_Informacion/Ordenacion_Busqueda.java
@@ -0,0 +1,12 @@
+package Analisis_Organizacion_Informacion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ordenacion_Busqueda {
+    private List<Venta> ventas;
+
+    public Ordenacion_Busqueda() {
+        ventas = new ArrayList<>();
+    }
+}
