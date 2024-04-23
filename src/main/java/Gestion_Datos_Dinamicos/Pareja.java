9cf3dc8858f0647159866b213e1850364731c67e
GabriHR <ghernrod@myuax.com>
2024-04-24 00:10:53 +0200

    5

---
+++ src/main/java/Gestion_Datos_Dinamicos/Pareja.java
@@ -0,0 +1,19 @@
+package Gestion_Datos_Dinamicos;
+
+public class Pareja {
+    private int primero;
+    private int segundo;
+
+    public Pareja(int primero, int segundo) {
+        this.primero = primero;
+        this.segundo = segundo;
+    }
+
+    public int getPrimero() {
+        return primero;
+    }
+
+    public int getSegundo() {
+        return segundo;
+    }
+}
