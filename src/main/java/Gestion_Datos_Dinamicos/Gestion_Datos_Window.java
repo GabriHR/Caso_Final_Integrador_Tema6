5dd2d37c11675dc0273443be5fe83deb6eaea269
GabriHR <ghernrod@myuax.com>
2024-04-24 00:13:37 +0200

    11

---
+++ src/main/java/Gestion_Datos_Dinamicos/Gestion_Datos_Window.java
@@ -0,0 +1,13 @@
+package Gestion_Datos_Dinamicos;
+
+import javax.swing.*;
+
+public class Gestion_Datos_Window extends JFrame {
+
+    private JTextArea textArea;
+    private ListaDatos listaDatos;
+    private JTextField realDataField;
+    private JTextField integerPairField1;
+    private JTextField integerPairField2;
+
+}
