ffb011ea2993ea8de1697531a72df818a8b00f49
GabriHR <ghernrod@myuax.com>
2024-04-24 00:27:05 +0200

    35

--- src/main/java/Mapas_Asociacion_Datos/Gestion_Relaciones.java
+++ src/main/java/Mapas_Asociacion_Datos/Gestion_Relaciones.java
@@ -1,8 +1,12 @@
 package Mapas_Asociacion_Datos;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Gestion_Relaciones {
     private Map<Integer, String> relaciones;
 
+    public Gestion_Relaciones() {
+        relaciones = new HashMap<>();
+    }
 }
