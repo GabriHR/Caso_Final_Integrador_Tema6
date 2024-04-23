f0e85ec7bcfe92bfe35db130d5156a0d42b435d0
GabriHR <ghernrod@myuax.com>
2024-04-24 00:22:14 +0200

    26

---
+++ src/main/java/Analisis_Organizacion_Informacion/Venta.java
@@ -0,0 +1,11 @@
+package Analisis_Organizacion_Informacion;
+
+import java.time.LocalDate;
+
+public class Venta {
+    private String cliente;
+    private String producto;
+    private double cantidad;
+    private LocalDate fecha;
+
+}
