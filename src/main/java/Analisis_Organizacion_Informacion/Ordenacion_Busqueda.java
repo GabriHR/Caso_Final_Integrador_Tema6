8f5fa4ab226e68cbf2f25da7a09c269a9f71b3e7
GabriHR <ghernrod@myuax.com>
2024-04-24 00:18:33 +0200

    18

--- src/main/java/Analisis_Organizacion_Informacion/Ordenacion_Busqueda.java
+++ src/main/java/Analisis_Organizacion_Informacion/Ordenacion_Busqueda.java
@@ -1,6 +1,7 @@
 package Analisis_Organizacion_Informacion;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Ordenacion_Busqueda {
@@ -8,5 +9,10 @@ public class Ordenacion_Busqueda {
 
     public Ordenacion_Busqueda() {
         ventas = new ArrayList<>();
+    }
+
+    public void agregarVenta(Venta venta) {
+        ventas.add(venta);
+        ventas.sort(Comparator.comparing(Venta::getFecha));
     }
 }
