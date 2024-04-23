0c01e809ab7b4f000d3b0e0b8dc6d1f206d800db
GabriHR <ghernrod@myuax.com>
2024-04-24 00:14:35 +0200

    12

--- src/main/java/Gestion_Datos_Dinamicos/Gestion_Datos_Window.java
+++ src/main/java/Gestion_Datos_Dinamicos/Gestion_Datos_Window.java
@@ -1,6 +1,7 @@
 package Gestion_Datos_Dinamicos;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Gestion_Datos_Window extends JFrame {
 
@@ -10,4 +11,19 @@ public class Gestion_Datos_Window extends JFrame {
     private JTextField integerPairField1;
     private JTextField integerPairField2;
 
+    public Gestion_Datos_Window(ListaDatos listaDatos) {
+        this.listaDatos = listaDatos;
+
+        setLayout(new BorderLayout());
+        textArea = new JTextArea();
+        add(new JScrollPane(textArea), BorderLayout.CENTER);
+
+        JPanel inputPanel = new JPanel();
+        inputPanel.setLayout(new GridLayout(3, 2));
+
+        realDataField = new JTextField();
+        integerPairField1 = new JTextField();
+        integerPairField2 = new JTextField();
+
+    }
 }
