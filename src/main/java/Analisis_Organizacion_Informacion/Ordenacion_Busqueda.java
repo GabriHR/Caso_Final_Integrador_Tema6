93a909489df0e815cacd908b8be984c39dee51f4
GabriHR <ghernrod@myuax.com>
2024-05-03 14:44:46 +0200

    92

--- src/main/java/Analisis_Organizacion_Informacion/Ordenacion_Busqueda.java
+++ src/main/java/Analisis_Organizacion_Informacion/Ordenacion_Busqueda.java
@@ -6,17 +6,28 @@ import java.util.List;
 
 public class Ordenacion_Busqueda {
     private List<Venta> ventas;
+    private List<Venta> originalVentas;
 
     public Ordenacion_Busqueda() {
         ventas = new ArrayList<>();
+        originalVentas = new ArrayList<>();
     }
 
     public void agregarVenta(Venta venta) {
         ventas.add(venta);
+        originalVentas.add(venta);
         ventas.sort(Comparator.comparing(Venta::getFecha));
     }
 
     public List<Venta> getVentasOrdenadas() {
         return ventas;
+    }
+
+    public void setVentasOrdenadas(List<Venta> ventasOrdenadas) {
+        this.ventas = ventasOrdenadas;
+    }
+
+    public void resetFilters() {
+        this.ventas = new ArrayList<>(originalVentas);
     }
 }
